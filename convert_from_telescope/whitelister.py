33a4b8170499bdefd08d78f7d10f8d70bb9fbd2d
Michael Lynch <mtlynch@google.com>
2015-03-19 08:30:02 -0400

    Converting from blacklisted datasets to whitelisted datasets and modifying implementation so that whitelisted datsets cannot become un-whitelisted once published.

--- convert_from_telescope/whitelister.py
+++ convert_from_telescope/whitelister.py
@@ -15,83 +15,155 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Check that Telescope datasets meet sample size thresholds."""
+"""Determine which input files should be processed for Observatory."""
 
 import logging
+import os
 
 import telescope_data_parser
 
 
-class DataFileBlacklister(object):
+class MetadataWhitelist(object):
+  """Whitelist of datasets based on metadata attributes."""
 
-  def __init__(self, sample_count_checker):
-    """Checks whether sample counts for given files meet the sample thresholds.
+  def __init__(self):
+    self._whitelisted_keys = set()
+
+  def __iter__(self):
+    for key in self._whitelisted_keys:
+      yield key
+
+  def is_whitelisted(self, site_name, isp):
+    """Indicates if the given dataset is whitelisted."""
+    key = self._dataset_key_from_metadata(site_name, isp)
+    return key in self._whitelisted_keys
+
+  def add(self, site_name, isp):
+    """Adds a dataset to the whitelist."""
+    key = self._dataset_key_from_metadata(site_name, isp)
+    self._whitelisted_keys.add(key)
+
+  def _dataset_key_from_metadata(self, site_name, isp):
+    """Derives a key for a particular dataset
+
+    Derives a whitelist key for a dataset based on metadata attributes.
 
     Args:
+      site_name: (str) The name of the M-Lab site (e.g. 'lga01').
+      isp: (str) The name of the client ISP (e.g. 'verizon').
+
+    Returns:
+      (str) Key of the form '[site]_[isp]', for example: 'lga01_verizon'.
+    """
+    return '%s_%s' % (site_name, isp)
+
+
+class MetadataWhitelistSerializer(object):
+  """Converts a whitelist to/from a file."""
+
+  def serialize(self, whitelist, output_file):
+    """Serialize a whitelist to a file.
+
+    Args:
+      whitelist: (MetadataWhitelist) Whitelist to serialize.
+      output_file: (file) File object to which to write the serialized data.
+    """
+    keys_sorted = sorted(whitelist)
+    output_file.write(os.linesep.join(keys_sorted))
+
+  def deserialize(self, whitelist_file):
+    """Parses a whitelist from a file.
+
+    Args:
+      whitelist_file: (file) File object from which to parse the whitelist.
+
+    Returns:
+      (MetadataWhitelist) Whitelist parsed from the file.
+    """
+    whitelist = MetadataWhitelist()
+    for line in whitelist_file:
+      site_name, isp = line.strip().split('_')
+      whitelist.add(site_name, isp)
+    return whitelist
+
+
+class MetadataWhitelistUpdater(object):
+  """Updates the whitelist with new datasets that meet sample requirements.
+
+  The update process keeps all the datasets that are currently in the whitelist
+  because Observatory should not unpublish datasets that have previously been
+  published. Updating checks all other datasets to see if there are new
+  datasets that now meet sample size thresholds (either because they did not
+  exist at the last check or their sample count has increased to meet
+  requirements).
+  """
+
+  def __init__(self, existing_whitelist, sample_count_checker):
+    """Creates a new whitelist updater.
+
+    Args:
+      existing_whitelist: (MetadataWhitelist) Current whitelist before adding
+        new datasets.
       sample_count_checker: (SampleCountChecker) Object to check whether
         datasets meet sample count thresholds.
     """
+    self.whitelist = existing_whitelist
     self._logger = logging.getLogger('telescope-convert')
     self._sample_count_checker = sample_count_checker
+    self._checked_datasets = {}
 
-  def populate(self, filenames):
-    """Adds the provided filenames to blacklist database.
-
-    Adds the provided file to the blacklist database by tracking
-    the total data counts in each dataset (note that this supports
-    adding multiple files to the same dataset and aggregating the
-    results).
+  def update(self, filenames):
+    """Updates whitelist by checking sample counts of provided datasets.
 
     Args:
-      filenames: (list) A list of filenames to add to blacklist database.
-    """
-    for filename in filenames:
-      self._logger.info('Analyzing %s for blacklist.', filename)
-      self._add_file(filename)
-
-  def is_blacklisted(self, filename):
-    """Indicates whether a file is part of a blacklisted dataset.
-
-    Args:
-      filename: (str) Filename to evaluate.
+      filenames: (list) A list of Telescope data files. Any datasets contained
+        in these files will be added to the whitelist if the dataset meets
+        sample count requirements.
 
     Returns:
-      (bool) True if the given filename is blacklisted because it is part
-      of a dataset that does not meet the sample size requirements.
+      (bool) True if datasets were added to the whitelist.
     """
-    result_reader = telescope_data_parser.SingleTelescopeResultReader(filename)
-    metadata = result_reader.get_metadata()
+    added_new_datasets = False
 
-    # This is a workaround because Observatory currently can't exclude datasets
-    # on a per-metric basis, so we use download_throughput as a lowest common
-    # denominator.
-    metadata['metric_name'] = 'download_throughput'
-    dataset_key = self._dataset_key_from_metadata(metadata)
+    # Check sample counts for all non-whitelisted datasets.
+    for filename in filenames:
+      self._check_file(filename)
 
-    return not self._sample_count_checker.has_enough_samples(dataset_key)
+    # Go through the analyzed datasets and add any datasets to the whitelist if
+    # they have sufficient samples.
+    for key, metadata in self._checked_datasets.iteritems():
+      if self._sample_count_checker.has_enough_samples(key):
+        site_name = metadata['site_name']
+        isp = metadata['isp']
+        self.whitelist.add(site_name, isp)
+        self._logger.info('Adding new dataset to whitelist: %s_%s', site_name,
+                          isp)
+        added_new_datasets = True
 
-  def _add_file(self, filename):
-    """Adds the provided filename to blacklist database.
+    return added_new_datasets
 
-    Adds the provided file to the blacklist database by tracking the total data
-    counts in each dataset (note that this supports adding multiple files to
-    the same dataset and aggregating the results).
+  def _check_file(self, filename):
+    """Analyze a data file to see if it should be whitelisted.
 
     Args:
-      filename: (str) Name of file to add to blacklist database.
+      filename: (str) Filename of Telescope data file to check.
     """
+    self._logger.info('Checking file for whitelist: %s', filename)
     result_reader = telescope_data_parser.SingleTelescopeResultReader(filename)
     metadata = result_reader.get_metadata()
 
-    # This is a performance optimization due to the workaround in
-    # is_blacklisted(). Because we don't use any metric except download
-    # throughput, there is no need to waste time parsing the data for other
-    # metrics.
+    # Because we don't use any metric except download throughput, there is no
+    # need to waste time parsing the data for other metrics.
     if metadata['metric_name'] != 'download_throughput':
+      return
+
+    # We don't need to check files that are already whitelisted.
+    if self.whitelist.is_whitelisted(metadata['site_name'], metadata['isp']):
       return
 
     dataset_key = self._dataset_key_from_metadata(metadata)
     self._sample_count_checker.add_to_counts(dataset_key, result_reader)
+    self._checked_datasets[dataset_key] = metadata
 
   def _dataset_key_from_metadata(self, metadata):
     """Derives a key for a particular dataset based on supplied metadata.
@@ -106,4 +178,31 @@ class DataFileBlacklister(object):
     dataset_key = '%s-%s-%s' % (metadata['site_name'], metadata['isp'],
                                 metadata['metric_name'])
     return dataset_key
+
+
+class DataFileWhitelistChecker(object):
+
+  def __init__(self, whitelist):
+    """Checks whether sample counts for given files meet the sample thresholds.
+
+    Args:
+      whitelist: (MetadataWhitelist) Whitelist to use to check files.
+    """
+    self._whitelist = whitelist
+
+  def is_whitelisted(self, filename):
+    """Indicates whether a file is part of a whitelisted dataset.
+
+    Args:
+      filename: (str) Filename to evaluate.
+
+    Returns:
+      (bool) True if the given filename is whitelisted because it is part of a
+      dataset that meets the sample size requirements.
+    """
+    result_reader = telescope_data_parser.SingleTelescopeResultReader(filename)
+    metadata = result_reader.get_metadata()
+
+    return self._whitelist.is_whitelisted(metadata['site_name'],
+                                          metadata['isp'])
 
--- convert_from_telescope/whitelister_test.py
+++ convert_from_telescope/whitelister_test.py
@@ -15,17 +15,74 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import io
 import unittest
 
 import mock
 
-import blacklister
+import whitelister
+
+
+class MetadataWhitelistTest(unittest.TestCase):
+
+  def test_is_whitelisted(self):
+    site_name = 'lga01'
+    isp = 'verizon'
+    whitelist = whitelister.MetadataWhitelist()
+
+    # Verify that the empty whitelist does not claim a dataset is whitelisted.
+    self.assertFalse(whitelist.is_whitelisted(site_name, isp))
+
+    # Verify that a dataset is whitelisted after being added to the whitelist.
+    whitelist.add(site_name, isp)
+    self.assertTrue(whitelist.is_whitelisted(site_name, isp))
+
+    # Verify that other datasets are not whitelisted.
+    self.assertFalse(whitelist.is_whitelisted('lax02', isp))
+    self.assertFalse(whitelist.is_whitelisted(site_name, 'twc'))
+
+
+class MetadataWhitelistSerializerTest(unittest.TestCase):
+
+  def setUp(self):
+    self._serializer = whitelister.MetadataWhitelistSerializer()
+    self._mock_file = io.BytesIO()
+
+  def test_serialize_empty_whitelist(self):
+    empty_whitelist = whitelister.MetadataWhitelist()
+    self._serializer.serialize(empty_whitelist, self._mock_file)
+    self.assertEqual('', self._mock_file.getvalue())
+
+  def test_serialize_basic_whitelist(self):
+    whitelist = whitelister.MetadataWhitelist()
+    whitelist.add('lga01', 'verizon')
+    whitelist.add('lga01', 'cablevision')
+    whitelist.add('lax02', 'twc')
+    whitelist.add('ord02', 'at&t')
+    self._serializer.serialize(whitelist, self._mock_file)
+    serialized_expected = ('lax02_twc\n'
+                           'lga01_cablevision\n'
+                           'lga01_verizon\n'
+                           'ord02_at&t')
+    self.assertEqual(serialized_expected, self._mock_file.getvalue())
+
+  def test_deserialize_basic_whitelist(self):
+    deserializer = whitelister.MetadataWhitelistSerializer()
+    self._mock_file = io.BytesIO(('lax02_twc\n'
+                                  'lga01_cablevision\n'
+                                  'lga01_verizon\n'
+                                  'ord02_at&t'))
+    whitelist = deserializer.deserialize(self._mock_file)
+    self.assertTrue(whitelist.is_whitelisted('lax02', 'twc'))
+    self.assertTrue(whitelist.is_whitelisted('lga01', 'cablevision'))
+    self.assertTrue(whitelist.is_whitelisted('lga01', 'verizon'))
+    self.assertTrue(whitelist.is_whitelisted('ord02', 'at&t'))
 
 
 def create_mock_reader(filename):
   """Creates a mock object to replace a TelescopeResultReader instance."""
   instance = mock.Mock()
-  filename_parts = filename.split('_')
+  filename_parts = filename.split('-')
   instance.get_metadata.return_value = {
       'site_name': filename_parts[0],
       'isp': filename_parts[1],
@@ -45,37 +102,83 @@ def mock_has_enough_samples(dataset_key):
   return mappings[dataset_key]
 
 
-class DataFileBlacklisterTest(unittest.TestCase):
+class MetadataWhitelistUpdaterTest(unittest.TestCase):
 
   @mock.patch('telescope_data_parser.SingleTelescopeResultReader')
-  def test_is_blacklisted(self, mock_result_reader):
+  def test_update_add_new(self, mock_result_reader):
+    """Verify that update() adds new datasets when they meet requirements."""
     mock_result_reader.side_effect = create_mock_reader
+
+    # Set the initial whitelist to contain lga01-verizon
+    whitelist = whitelister.MetadataWhitelist()
+    whitelist.add('lga01', 'verizon')
 
     sample_checker = mock.Mock()
     sample_checker.has_enough_samples.side_effect = mock_has_enough_samples
-    mock_filenames = ('lga01_comcast_download_throughput_1.csv',
-                      'lga01_comcast_download_throughput_2.csv',
-                      'lga01_verizon_download_throughput_1.csv',
-                      'mia02_comcast_download_throughput_1.csv',
-                      'sea05_twc_download_throughput_1.csv')
-    file_blacklister = blacklister.DataFileBlacklister(sample_checker)
-    file_blacklister.populate(mock_filenames)
+    mock_filenames = ('lga01-comcast-download_throughput-1.csv',
+                      'lga01-comcast-download_throughput-2.csv',
+                      'lga01-verizon-download_throughput-1.csv',
+                      'mia02-comcast-download_throughput-1.csv',
+                      'sea05-twc-download_throughput-1.csv')
 
-    # These SHOULD NOT be blacklisted, as the mock sample checker says their
-    # datasets have sufficient samples.
-    self.assertFalse(file_blacklister.is_blacklisted(
-        'lga01_comcast_download_throughput_1.csv'))
-    self.assertFalse(file_blacklister.is_blacklisted(
-        'lga01_comcast_download_throughput_2.csv'))
-    self.assertFalse(file_blacklister.is_blacklisted(
-        'lga01_verizon_download_throughput_1.csv'))
+    updater = whitelister.MetadataWhitelistUpdater(whitelist, sample_checker)
+    self.assertTrue(updater.update(mock_filenames))
 
-    # These SHOULD be blacklisted, as the mock sample checker says their
-    # datasets do not have sufficient samples.
-    self.assertTrue(file_blacklister.is_blacklisted(
-        'mia02_comcast_download_throughput_1.csv'))
-    self.assertTrue(file_blacklister.is_blacklisted(
-        'sea05_twc_download_throughput_1.csv'))
+    # lga01-comcast has enough samples, so it should be added to the whitelist.
+    self.assertTrue(whitelist.is_whitelisted('lga01', 'comcast'))
+
+    # lga01-verizon was already in the whitelist and should remain there.
+    self.assertTrue(whitelist.is_whitelisted('lga01', 'verizon'))
+
+    # mia02-comcast and sea05-twc do not have enough samples and should not be
+    # in the whitelist.
+    self.assertFalse(whitelist.is_whitelisted('mia02', 'comcast'))
+    self.assertFalse(whitelist.is_whitelisted('sea05', 'twc'))
+
+  @mock.patch('telescope_data_parser.SingleTelescopeResultReader')
+  def test_update_maintain_existing(self, mock_result_reader):
+    """Verify that update does not remove datasets from the whitelist."""
+    mock_result_reader.side_effect = create_mock_reader
+
+    # Set the initial whitelist to contain sea05-twc
+    whitelist = whitelister.MetadataWhitelist()
+    whitelist.add('sea05', 'twc')
+
+    sample_checker = mock.Mock()
+    sample_checker.has_enough_samples.side_effect = mock_has_enough_samples
+    mock_filenames = ('sea05-twc-download_throughput-1.csv',)
+
+    updater = whitelister.MetadataWhitelistUpdater(whitelist, sample_checker)
+    updater.update(mock_filenames)
+
+    # Even though sea05-twc does not meet sample size requirements, it was
+    # initially in the whitelist and therefore should remain whitelisted.
+    self.assertTrue(whitelist.is_whitelisted('sea05', 'twc'))
+
+
+class DataFileWhitelistCheckerTest(unittest.TestCase):
+
+  @mock.patch('telescope_data_parser.SingleTelescopeResultReader')
+  def test_is_whitelisted(self, mock_result_reader):
+    mock_result_reader.side_effect = create_mock_reader
+
+    whitelist = whitelister.MetadataWhitelist()
+    whitelist.add('lga01', 'verizon')
+    whitelist.add('nuq01', 'comcast')
+
+    checker = whitelister.DataFileWhitelistChecker(whitelist)
+    # This file should not be whitelisted as lga01-comcast is not whitelisted.
+    self.assertFalse(checker.is_whitelisted(
+        'lga01-comcast-download_throughput-1.csv'))
+
+    # These files should be whitelisted because their associated metadata
+    # attributes are whitelisted.
+    self.assertTrue(checker.is_whitelisted(
+        'lga01-verizon-download_throughput-1.csv'))
+    self.assertTrue(checker.is_whitelisted(
+        'lga01-verizon-minimum_rtt-1.csv'))
+    self.assertTrue(checker.is_whitelisted(
+        'nuq01-comcast-download_throughput-3.csv'))
 
 
 if __name__ == '__main__':
